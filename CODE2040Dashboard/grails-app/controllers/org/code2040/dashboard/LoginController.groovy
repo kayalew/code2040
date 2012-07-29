0ddf7c43ba247b571ced4633cd4684af8c4c4b33
Kaleb Ayalew <kayalew@mit.edu>
2012-07-29 08:40:25 -0700

    added basic logincontroller/loginservice

--- CODE2040Dashboard/grails-app/controllers/org/code2040/dashboard/LoginController.groovy
+++ CODE2040Dashboard/grails-app/controllers/org/code2040/dashboard/LoginController.groovy
@@ -1,14 +1,38 @@
 package org.code2040.dashboard
 
+import org.code2040.dashboard.services.SecurityService
+import org.code2040.dashboard.services.LoginService
+
 class LoginController {
+	
+	boolean staff = false
+	def loginService
 
     def index() {
 		def htmlContent = new File('grails-app/views/login.html').text
 		render text: htmlContent, contentType:"text/html", encoding:"UTF-8"
+		staff = false
 	}
 	
 	def staff() {
 		def htmlContent = new File('grails-app/views/staff-login.html').text
 		render text: htmlContent, contentType:"text/html", encoding:"UTF-8"
+		staff = true
+	}
+	
+	def update() {
+		String lookupEmail = params.email
+		String possiblePassword = params.password
+		String goodHash = new String()
+		if (!staff){
+			//goodHash =  mysqldb.query("SELECT hash FROM students WHERE email="+lookupEmail)
+			//loginService.checkEmptyUser(goodHash, staff)
+			//loginService.processPassword(possiblePassword, goodHash, staff)
+		}
+		else{
+			//goodHash =  mysqldb.query("SELECT hash FROM staff WHERE email="+lookupEmail)
+			//loginService.checkEmptyUser(goodHash, staff)
+			//loginService.processPassword(possiblePassword, goodHash, staff)
+		}
 	}
 }
